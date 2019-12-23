6c2eb030a25051fe6595999f0c853a3e10d22183
George Ostrouchov <ostrouchovg@utk.edu>
2019-12-23 00:54:35 -0500

    slurm debug

--- hello_balance.R
+++ hello_balance.R
@@ -9,10 +9,11 @@ mc.function = function(x) {
 }
 
 ## Compute how many cores per R session are on this node
-ranks_per_node = as.numeric(system("echo $OMPI_COMM_WORLD_LOCAL_SIZE",
+ranks_on_my_node = as.numeric(system("echo $OMPI_COMM_WORLD_LOCAL_SIZE",
                                    intern = TRUE))
 cores_on_my_node = detectCores()
-cores_per_R = floor(cores_on_my_node/ranks_per_node)
+cores_total = allreduce(cores_on_my_node)
+cores_per_R = floor(cores_on_my_node/ranks_on_my_node)
 
 ## Run lapply on allocated cores to demonstrate fork pids
 my_pids = mclapply(1:cores_per_R, mc.function, mc.cores = cores_per_R)
