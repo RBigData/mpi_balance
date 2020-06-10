c54d53033c0b8cdb0b36bd98ecd61fb2390dd4c4
Ostrouchov G <ost@or-slurm-login08.ornl.gov>
2020-06-10 16:58:01 -0400

    slurm migration

--- hello_balance.R
+++ hello_balance.R
@@ -11,9 +11,10 @@ mc.function = function(x) {
 ## Compute how many cores per R session are on this node
 ranks_on_my_node = as.numeric(system("echo $OMPI_COMM_WORLD_LOCAL_SIZE",
                                    intern = TRUE))
+
 cores_on_my_node = detectCores()
-cores_total = allreduce(cores_on_my_node)
 cores_per_R = floor(cores_on_my_node/ranks_on_my_node)
+cores_total = allreduce(cores_per_R)
 
 ## Run lapply on allocated cores to demonstrate fork pids
 my_pids = mclapply(1:cores_per_R, mc.function, mc.cores = cores_per_R)
@@ -25,14 +26,19 @@ my_pids = do.call(paste, my_pids) # combines results from mclapply
 ##            same cores: avoid or manage appropriately.
 
 ## Now report what happened and where
-msg = paste0("Hello World from rank ", comm.rank(), " on host ", host, "\n",
-             "      with ", cores_per_R, " cores allocated (", ranks_on_my_node,
-             " R sessions sharing ", cores_on_my_node, " cores).\n",
+msg = paste0("Hello World from rank ", comm.rank(), " on host ", host,
+             " with ", cores_per_R, " cores allocated\n",
+             "            (", ranks_on_my_node, " R sessions sharing ",
+             cores_on_my_node, " cores on this host node).\n",
              "      pid: ", my_pids, "\n")
 comm.cat(msg, quiet = TRUE, all.rank = TRUE)
 
 
 comm.cat("Total R sessions:", comm.size(), "Total cores:", cores_total, "\n",
+         quiet = TRUE)
+comm.cat("\nNotes: cores on node obtained by: detectCores {parallel}\n",
+         "       ranks (R sessions) per node: OMPI_COMM_WORLD_LOCAL_SIZE\n",
+         "       pid to core map changes frequently during mclapply\n",
          quiet = TRUE)
 
 finalize()
