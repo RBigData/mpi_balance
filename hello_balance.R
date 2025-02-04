8807018d7aa7fdc5e3c7cda18f0ad163b62e629d
George Ostrouchov <gostrouc@utk.edu>
2025-02-04 15:40:48 -0500

    add all times

--- hello_balance.R
+++ hello_balance.R
@@ -13,6 +13,10 @@ format_pids = function(x) {
         paste(formatC(x - u*10^i, width = i, format = "d", flag = "0"), collapse = " "))
 }
 
+## Small fraction sleep for print synchronization. When zero, some printing may be 
+## out of order due to different paths from different nodes.
+sleep = 0.1
+
 ## list R session info from rank 0 while others wait
 if(comm.rank() == 0) {
   sessionInfo()
@@ -26,7 +30,7 @@ rank = comm.rank()
 size = comm.size()
 
 mc.function = function(x) {
-    Sys.sleep(0.1) # replace with your function for mclapply cores here
+    Sys.sleep(sleep) # replace with your function for mclapply cores here
     Sys.getpid() # returns process id
 }
 
@@ -42,8 +46,8 @@ cores_total = allreduce(cores_per_R)  # adds up over ranks
 barrier()
 mc_time = system.time({
 my_mcpids = parallel::mclapply(1:cores_per_R, mc.function, mc.cores = cores_per_R)
-barrier()
 })
+mc_times = gather(mc_time[3]) # gather each rank's time to rank 0
 
 ## Run lapply this time with same function
 l_time = system.time({
@@ -63,21 +67,17 @@ msg = paste0("Hello from rank ", rank, " on node ", host, " claiming ",
              "      pid: ", format_pids(my_mcpids), "\n")
 comm.cat(msg, quiet = TRUE, all.rank = TRUE)
 
-Sys.sleep(0.1) ## grace for all Hellos to propagate out
-barrier() ## wait for all ranks to finish Hellos, then start writing summary by rank 0
-comm.cat("\nTotal R sessions:", size, "\n", quiet = TRUE)
-comm.cat("Total cores:", cores_total, "\n", quiet = TRUE)
+Sys.sleep(sleep) ## grace for all Hellos to propagate from nodes
+barrier() ## wait to finish all Hellos, then start writing summary by rank 0
+comm.cat("\nTotal R sessions:", size, "   Total cores:", cores_total, "\n", quiet = TRUE)
 comm.cat("\nNotes: cores on node obtained by: detectCores {parallel}\n",
          "       ranks (R sessions) per node: OMPI_COMM_WORLD_LOCAL_SIZE\n",
          "       pid to core map changes frequently during mclapply\n",
          quiet = TRUE)
 
-comm.cat("\nTime lapply should be about", cores_per_R, "x time mclapply\n", 
-         quiet = TRUE)
-comm.cat("     mclapply time on each of the", size, "ranks:", mc_time[3], "\n", 
-         quiet = TRUE)
-comm.cat("     lapply time on each of the", size, "ranks:", l_time[3], "\n\n", 
-         quiet = TRUE)
+comm.cat("\nTime lapply should be about", cores_per_R, "x time mclapply\n", quiet = TRUE)
+comm.cat("     mclapply time on each of the", size, "ranks:", paste(mc_times, collapse = " "), "\n", quiet = TRUE)
+comm.cat("     lapply time on each of the", size, "ranks:", l_time[3], "\n\n", quiet = TRUE)
 
 finalize()
 
